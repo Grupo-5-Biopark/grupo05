dbaf79979344e33bdecba76f7f19346a21d7ce25
João Victor Haas <131174989+haaszdev@users.noreply.github.com>
2025-09-04 19:09:08 -0300

    feat: Create base front-end structure (#32) (#36)
    * feat: Create base structure with the default folders and NestJs initial files
    
    * feat: Create base structure with the default folders and NextJs initial files
    
    ---------
    
    Co-authored-by: Lucas-Dreveck <lucasdrevc@outlook.com>

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'BIOPARK - Sistema de Controle de Salas',
+  description: 'Sistema de gerenciamento de crescimento da faculdade para estruturação e organização de salas',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
