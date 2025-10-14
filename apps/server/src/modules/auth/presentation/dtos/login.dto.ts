0fb947a94b5f7eeabf20afe110593533c3bb082f
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2025-10-14 19:17:46 -0300

    Login exemple in swagger

--- apps/server/src/modules/auth/presentation/dtos/login.dto.ts
+++ apps/server/src/modules/auth/presentation/dtos/login.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({ example: 'admin@admin.com' })
   @IsEmail({}, { message: 'must be a valid email' })
   @IsNotEmpty({ message: 'cannot be empty' })
   email: string;
 
+  @ApiProperty({ example: 'admin' })
   @IsString({ message: 'must be a string' })
   @IsNotEmpty({ message: 'cannot be empty' })
   password: string;
