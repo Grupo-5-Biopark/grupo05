f3c2c6e6f1b8bcffd7b23e063573c2b1f369dcef
heloshartmann <133721438+heloshartmann@users.noreply.github.com>
2025-10-22 19:20:10 -0300

    solving cors problem

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -8,6 +8,11 @@ import { JwtAuthGuard } from './modules/auth/infrastructure/guards/jwt-auth.guar
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Biopark Room Control API')
