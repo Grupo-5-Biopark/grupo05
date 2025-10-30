b74ffbdc255833f3b5297a2ee31ef58e61f4e721
heloshartmann <133721438+heloshartmann@users.noreply.github.com>
2025-10-30 15:09:18 -0300

    arrumando problema de mostrar a tela repetida

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -6,7 +6,6 @@ import { useAuth } from '../../hooks/useAuth';
 import Header from '../../components/ui/Header';
 import Sidebar from '../../components/ui/Sidebar';
 import './dashboard.css';
-import UsersPage from '../users/page';
 
 export default function DashboardPage() {
   const router = useRouter();
@@ -159,11 +158,6 @@ export default function DashboardPage() {
               previsões semestrais.
             </p>
           </div>
-        </div>
-
-        {/* USUÁRIOS PAGE */}
-        <div className={`page ${currentPage === 'usuarios' ? 'active' : ''}`}>
-          <UsersPage />
         </div>
       </main>
     </div>
